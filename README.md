f691b4502dea45d7a7e9e2a11dcb1b5fdec7da25
Reberti Carvalho Soares <rebertics@gmail.com>
2026-06-13 20:52:08 -0300

    Closes #1,#2,#3: Added docker-build.py

--- README.md
+++ README.md
@@ -2,19 +2,49 @@
 Simple bash script to backup docker volumes using a list of containers
 
 
+
+
+## Usage
+### Python Script:
+Create `.env` file containing:
+
+```bash
+CONTAINER_LIST="CONTAINER_A CONTAINER_B CONTAINER_C"
+BACKUP_PATH="<BACKUP_PATH>"
+```
+
+#### Running
+```bash
+chmod +x deenesse
+./deenesse [configuration]
+```
+
+## Systemd
+Create `/etc/docker-backup/production.conf` file containing:
+
+```bash
+CONTAINER_LIST="CONTAINER_A CONTAINER_B CONTAINER_C"
+BACKUP_PATH="<BACKUP_PATH>"
+```
+
+#### Enable Timer
+```bash
+sudo systemctl enable docker-backup@production.timer
+```
+
+### Bash Script
 ## Install
 ``` bash
 curl -O https://raw.githubusercontent.com/RebertiCS/Docker-Backup/refs/heads/main/docker-backup.sh
 chmod +x docker-backup.sh
 ```
 
-## Usage
-### Command
+#### Command
 ``` bash
 bash dboard-backup.sh ./container_list.txt /backup/volumes/
 ```
 
-### Crontab
+#### Crontab
 Runs everyday at 12 PM
 ``` bash
 0 0 * * * /usr/bin/bash /root/dboard-backup.sh /root/container_list.txt /home/rebertics/backups/ag_00/volumes/
---
+++ docker-backup.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python3
+"""Docker volume backup"""
+import os
+import re
+import sys
+import subprocess
+from datetime import datetime
+
+from dotenv import load_dotenv
+
+def main():
+    """Docker backup start"""
+    if len(sys.argv) > 1:
+        load_dotenv(sys.argv[1])
+    else:
+        load_dotenv()
+
+    try:
+        container_list = os.getenv("CONTAINER_LIST").split(" ")
+    except KeyError:
+        print("Missing CONTAINER_LIST enviromental variable")
+        raise
+
+    for container in container_list:
+        create_backup(container)
+
+
+    return 0
+
+def create_backup(container):
+
+    backup_dir = None
+
+    try:
+        backup_dir = os.getenv("BACKUP_PATH")
+    except KeyError:
+        print("Missing BACKUP_PATH enviromental variabel")
+        raise
+
+    date_str = datetime.now().strftime('%Y-%m-%d_%H%M%S')
+
+    volume_list = get_volumes(container)
+
+    for volume_path in volume_list:
+        volume_name = re.split("/", volume_path)[-2]
+
+        command = [
+            "tar",
+            "-Jcf",
+            f"{backup_dir}/{container}/{volume_name}-{date_str}.tar.xz",
+            "-C",
+            f"{volume_path}",
+            "."
+        ]
+
+        # Make backup dir
+        subprocess.run(["mkdir", "-p", f"{backup_dir}/{container}"])
+
+        # Stop container
+        subprocess.run(["docker", "stop", f"{container}"])
+
+        # Run backup
+        subprocess.run(command)
+
+        # Start container
+        subprocess.run(["docker", "start", f"{container}"])
+
+
+def get_volumes(container):
+    cmd_list = [
+        "docker",
+        "inspect",
+        "-f",
+        "'{{ range .Mounts }}{{ .Source }} {{ end }}'",
+        f"{container}"
+    ]
+
+    volume_list = (
+        subprocess
+        .run(
+            cmd_list,
+            capture_output=True,
+            text=True)
+        .stdout
+        .replace(" '\n", "")
+        .replace("'", "")
+        .split(" ")
+    )
+
+    return volume_list
+
+
+
+
+
+if __name__ == "__main__":
+    main()
